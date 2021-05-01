5627b8e83a5a8c8fe4ad135f37d3f238a04acc9a
akhilcjose <akhiljose1999@gmail.com>
2021-05-01 19:43:45 +0530

    user profile

--- my_project1/urls.py
+++ my_project1/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,8 @@ urlpatterns = [
     path('', include('blog.urls')),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
