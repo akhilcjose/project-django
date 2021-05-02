c42b3ccae9682fa5d7be3767ab0d62d8400620ce
akhilcjose <akhiljose1999@gmail.com>
2021-05-03 00:08:29 +0530

    add blog

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class post(models.Model):
     title = models.CharField(max_length=20)
@@ -8,3 +9,7 @@ class post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import post
 
-def home(request):
-    context = {'posts': post.objects.all()}
-    return render(request, 'blog/home.html', context)
+#def home(request):
+ #   context = {'posts': post.objects.all()}
+  #  return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    
+class PostDetailView(DetailView):
+    model = post
+
+class PostCreateView(CreateView):
+    model = post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.name = self.request.user
+        return super().form_valid(form)
+    
+    
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
 
