5e5a2fb73185b3c3bfd35bf6bc1e11ccb8364a26
akhilcjose <akhiljose1999@gmail.com>
2021-05-03 22:14:07 +0530

    blog updates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import post
+
 
 #def home(request):
  #   context = {'posts': post.objects.all()}
@@ -14,13 +16,36 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = post
     fields = ['title', 'content']
     
     def form_valid(self, form):
         form.instance.name = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.name = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.name:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = post
+    success_url = '/'
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.name:
+            return True
+        return False
     
     
 def about(request):
